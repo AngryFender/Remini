602220c8e18885bb353aeb1ea30e59f948e7cafe
AngryFender <TeaBiscuitPerson@gmail.com>
2023-06-17 23:52:35 +0100

    Github Issue #64: Added more test for mktextdocument

--- CatchTests/mktextdocument_tests.cpp
+++ CatchTests/mktextdocument_tests.cpp
@@ -82,3 +82,45 @@ TEST_CASE("MkTextDocument single word italic plus false italic sign, hide only i
     QString text = edit.toPlainText();
     REQUIRE("abc *" == text);
 }
+
+TEST_CASE("MkTextDocument link texts", "[MkTextDocument]")
+{
+    MkTextDocument doc;
+    MkEdit edit;
+    QRect rec;
+
+    doc.setPlainText("[youtube](www.youtube.com)");
+    edit.setDocument(&doc);
+    doc.cursorPosChangedHandle(false, 1,rec);
+
+    QString text = edit.toPlainText();
+    REQUIRE("youtube" == text);
+}
+
+TEST_CASE("MkTextDocument link texts with underscore", "[MkTextDocument]")
+{
+    MkTextDocument doc;
+    MkEdit edit;
+    QRect rec;
+
+    doc.setPlainText("[lang_explain](https://sqlite.org/lang_explain.html)");
+    edit.setDocument(&doc);
+    doc.cursorPosChangedHandle(false, 1,rec);
+
+    QString text = edit.toPlainText();
+    REQUIRE("lang_explain" == text);
+}
+
+TEST_CASE("MkTextDocument link texts with empty title", "[MkTextDocument]")
+{
+    MkTextDocument doc;
+    MkEdit edit;
+    QRect rec;
+
+    doc.setPlainText("[](www.google.com)");
+    edit.setDocument(&doc);
+    doc.cursorPosChangedHandle(false, 1,rec);
+
+    QString text = edit.toPlainText();
+    REQUIRE("" == text);
+}
