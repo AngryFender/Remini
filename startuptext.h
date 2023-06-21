22e08fb622be47d20ec0eeb64bbfc22a583745d3
AngryFender <TeaBiscuitPerson@gmail.com>
2023-06-21 23:19:07 +0100

    Github Issue #61: Added a startup text for keyboard shortcut and config file

---
+++ startuptext.h
@@ -0,0 +1,20 @@
+#ifndef STARTUPTEXT_H
+#define STARTUPTEXT_H
+
+#include <QString>
+
+const QString startupText = QStringLiteral("Keyboard Shortcuts:\n"
+                                           " - Alt + 1 to search filenames\n"
+                                           " - Alt + 2 to navigate files/folders\n"
+                                           " - Alt + 3 to start editing files\n"
+                                           " - Quick double shift for searching words in all files\n"
+                                           " - \"ch\" or \"check\" followed by tab for checkmark\n"
+                                           " - \"uch\" or \"uncheck\" followed by tab for empty box\n"
+                                           " - \"link\" or \"lk\"followed by tab for link markdown symbols\n"
+                                           "\n"
+                                           "Path Config:\n"
+                                           " - Check config.txt in the exe folder to update the root path\n"
+                                           "\n"
+                                           );
+
+#endif // STARTUPTEXT_H
