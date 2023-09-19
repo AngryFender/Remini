0a0864f1c0dd672bd6654e5135ccc6007dcd7462
AngryFender <TeaBiscuitPerson@gmail.com>
2023-09-19 23:49:25 +0100

    Github Issue #39: Added Dialog widget for settings menu

---
+++ settingsdialog.cpp
@@ -0,0 +1,6 @@
+#include "settingsdialog.h"
+
+SettingsDialog::SettingsDialog()
+{
+
+}
---
+++ settingsdialog.h
@@ -0,0 +1,14 @@
+#ifndef SETTINGSDIALOG_H
+#define SETTINGSDIALOG_H
+
+#include <QDialog>
+#include <QObject>
+#include <QWidget>
+
+class SettingsDialog : public QDialog
+{
+public:
+    SettingsDialog();
+};
+
+#endif // SETTINGSDIALOG_H
