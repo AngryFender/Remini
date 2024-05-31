ca7738bcb3a25c21fcef9368475ab145635cddf3
AngryFender <TeaBiscuitPerson@gmail.com>
2024-05-31 21:44:33 +0100

    Github Issue #170: Added steps to save the file immediately if pressing backspace causing merging of mutliple blocks

--- mkedit.cpp
+++ mkedit.cpp
@@ -185,6 +185,7 @@ void MkEdit::keyPressEvent(QKeyEvent *event)
     case Qt::Key_Return:    emit enterKeyPressed(this->textCursor().blockNumber());
     case Qt::Key_Space:     fileSaveNow(); return;
     case Qt::Key_QuoteLeft: quoteLeftKey(); fileSaveNow(); return;
+    case Qt::Key_Backspace: if( undoData.forceMulti == true){ fileSaveNow(); return;}break;
     case Qt::Key_D:         if( event->modifiers() == Qt::CTRL) {emit duplicateLine(this->textCursor().blockNumber());; fileSaveNow(); return;}break;
     case Qt::Key_Z:         if( event->modifiers() == Qt::CTRL) {emit undoStackUndoSignal(); undoData.undoRedoSkip = true; fileSaveNow();showSelectionAfterUndo(); return;}break;
     case Qt::Key_Y:         if( event->modifiers() == Qt::CTRL) {emit undoStackRedoSignal(); undoData.undoRedoSkip = true; fileSaveNow();showSelectionAfterRedo(); return;}break;
