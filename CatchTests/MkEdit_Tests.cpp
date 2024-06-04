f380d8bf9302d28cb1cd28bff82356972bd789a0
AngryFender <TeaBiscuitPerson@gmail.com>
2024-06-04 23:29:32 +0100

    Github Issue #165: Added unit test to check if the creating and editing code block behaves properly

--- CatchTests/MkEdit_Tests.cpp
+++ CatchTests/MkEdit_Tests.cpp
@@ -1495,6 +1495,73 @@ TEST_CASE("MkEdit create code block with ```, undo/redo", "[MkEdit]")
     REQUIRE("```\n```" == text);
 }
 
+TEST_CASE("MkEdit pressing enter after creating code block with ```, undo/redo", "[MkEdit]")
+{
+    MkTextDocument doc;
+    MkEdit edit;
+
+    edit.setDocument(&doc);
+
+    QObject::connect(&edit,&MkEdit::cursorPosChanged,
+                     &doc,&MkTextDocument::cursorPosChangedHandle);
+
+    QObject::connect(&edit,&MkEdit::removeAllMkData,
+                     &doc,&MkTextDocument::removeAllMkDataHandle);
+
+    QObject::connect(&edit,&MkEdit::applyAllMkData,
+                     &doc,&MkTextDocument::applyAllMkDataHandle);
+
+    QObject::connect(&edit,&MkEdit::undoStackPushSignal,
+                     &doc,&MkTextDocument::undoStackPush);
+
+    QObject::connect(&edit,&MkEdit::undoStackUndoSignal,
+                     &doc,&MkTextDocument::undoStackUndo);
+
+    QObject::connect(&edit,&MkEdit::undoStackRedoSignal,
+                     &doc,&MkTextDocument::undoStackRedo);
+
+    QObject::connect(&edit,&MkEdit::saveSingleRawBlock,
+                     &doc,&MkTextDocument::saveSingleRawBlockHandler);
+
+    QObject::connect(&edit,&MkEdit::saveRawDocument,
+                     &doc,&MkTextDocument::saveRawDocumentHandler);
+
+    QObject::connect(&edit,&MkEdit::enterKeyPressed,
+                     &doc,&MkTextDocument::enterKeyPressedHandle);
+
+    QObject::connect(&edit,&MkEdit::quoteLeftKeyPressed,
+                     &doc,&MkTextDocument::quoteLeftKeyPressedHandle);
+
+    QScopedPointer<QKeyEvent> keyPressEvent (new QKeyEvent(QEvent::KeyPress, Qt::Key_QuoteLeft, Qt::NoModifier,QString("`")));
+    edit.keyPressEvent(keyPressEvent.data());
+    edit.keyPressEvent(keyPressEvent.data());
+    edit.keyPressEvent(keyPressEvent.data());
+
+    QString text = edit.toPlainText();
+    text = edit.toPlainText();
+    REQUIRE("```\n```" == text);
+
+    keyPressEvent.reset(new QKeyEvent(QEvent::KeyPress, Qt::Key_Enter, Qt::NoModifier));
+    edit.keyPressEvent(keyPressEvent.data());
+    text = edit.toPlainText();
+    REQUIRE("```\n\n```" == text);
+
+    QScopedPointer<QKeyEvent> undoKeyPressEvent (new QKeyEvent(QEvent::KeyPress, Qt::Key_Z, Qt::ControlModifier));
+    edit.keyPressEvent(undoKeyPressEvent.data());
+    text = edit.toPlainText();
+    REQUIRE("```\n```" == text);
+
+    edit.keyPressEvent(undoKeyPressEvent.data());
+    text = edit.toPlainText();
+    REQUIRE("``" == text);
+
+    QScopedPointer<QKeyEvent>  redoKeyPressEvent(new QKeyEvent(QEvent::KeyPress, Qt::Key_Y, Qt::ControlModifier)) ;
+    edit.keyPressEvent(redoKeyPressEvent.data());
+
+    text = edit.toPlainText();
+    REQUIRE("```\n```" == text);
+}
+
 TEST_CASE("MkEdit pressing delete as the end of the text block, undo/redo", "[MkEdit]")
 {
     MkTextDocument doc;
