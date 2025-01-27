7d72558fe554767002c8b2903740bb8aaf06158c
AngryFender <TeaBiscuitPerson@gmail.com>
2025-01-27 22:20:31 +0000

    Github Issue #218: Started drawing outline of the toggle switch

--- togglebutton.cpp
+++ togglebutton.cpp
@@ -4,6 +4,8 @@
 ToggleButton::ToggleButton(QWidget *parent):QAbstractButton(parent), animation(new QPropertyAnimation(this,"pos",this))
 {
     animation->setDuration(200);
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(QColorConstants::Blue);
 }
 
 void ToggleButton::keyPressEvent(QKeyEvent *e)
@@ -36,6 +38,11 @@ void ToggleButton::paintEvent(QPaintEvent *e)
     QPainter p(this);
     p.setBrush(state? brush:Qt::gray);
     p.setRenderHint(QPainter::Antialiasing, true);
-    p.drawRoundedRect(QRect(3,3,width()-2* 3, height()-2*3), 7.0,7.0);
+    p.drawRoundedRect(QRect(5,height()/2,width()-2* 3, height()/3), 4,5);
+
+    p.setBrush(circle);
+    p.setBrush(state? brush:Qt::gray);
+    p.setRenderHint(QPainter::Antialiasing, true);
+    p.drawEllipse(QPoint(12,height()/3*2),7,7);
 
 }
